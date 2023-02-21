db825a5967ef3633a82952fa4e2898a0cc735599
Gegham <geghamharutyunyan1994t@gmail.com>
2023-02-21 19:49:48 +0400

    fix header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'my-profile', component: MyProfileComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    MyProfileComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
